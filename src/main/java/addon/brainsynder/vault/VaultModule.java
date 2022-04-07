10f0ba4fc81952c21d274df61589f06059579d2d
brainsynder <briansnyder498@gmail.com>
2022-04-07 18:53:10 -0400

    Updated addon.json

--- src/main/java/addon/brainsynder/vault/VaultModule.java
+++ src/main/java/addon/brainsynder/vault/VaultModule.java
@@ -2,26 +2,15 @@ package addon.brainsynder.vault;
 
 import net.milkbowl.vault.economy.Economy;
 import org.bukkit.Bukkit;
-import org.bukkit.plugin.Plugin;
 import org.bukkit.plugin.RegisteredServiceProvider;
 import simplepets.brainsynder.addon.presets.EconomyModule;
 import simplepets.brainsynder.api.Namespace;
-import simplepets.brainsynder.api.plugin.SimplePets;
 
 import java.util.UUID;
 
 @Namespace(namespace = "Vault")
 public class VaultModule extends EconomyModule {
     private Economy econ = null;
-
-    @Override
-    public boolean shouldEnable() {
-        Plugin plugin = Bukkit.getPluginManager().getPlugin("Vault");
-        if ((plugin != null) && plugin.isEnabled()) return true;
-        SimplePets.getDebugLogger().debug(SimplePets.ADDON, "You seem to be missing the Vault plugin...");
-        SimplePets.getDebugLogger().debug(SimplePets.ADDON, "Download it here: https://www.spigotmc.org/resources/34315/");
-        return false;
-    }
 
     @Override
     public void init() {
